633c52ac912b4f16d3dbe8dbaa96bedc72dd8718
Hamza <hamza.peracha@econommi.io>
2022-06-17 11:19:23 -0700

    fix: clean

--- test-changelog.config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const config = require('conventional-changelog-conventionalcommits');
-
-module.exports = config({
-    "types": [
-        { type: 'feat', section: 'New Features' },
-        { type: 'fix', section: 'Bugs' }
-    ]
-})
